61680c7cdcd95f8f99fff829f2adc9842e36e466
Rishabh Dev Nayak <rdnayak26@gmail.com>
2024-04-04 18:35:26 +0530

    Minor Updates

--- Assets/Scripts/Cannon_Projectile.cs
+++ Assets/Scripts/Cannon_Projectile.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Cannon_Projectile : MonoBehaviour
 {
+     
     public Rigidbody rb;
     public float y, z;
-    public float Power;
+    private float Power = 0f;
+    private float scrollspeed = 0.2f;
+
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float scrollinput = 10 * Input.GetAxis("Mouse ScrollWheel");
+
+        if ( scrollinput > 0) {
+            Power += scrollinput * scrollspeed;
+            Debug.LogError(scrollinput);
+        }
+        if (scrollinput < 0) {
+            Power += scrollinput * scrollspeed;
+            Debug.LogError(scrollinput);
+        }
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
             rb.AddForce(new Vector3(0, y*Power, z*Power), ForceMode.Impulse);
         }
