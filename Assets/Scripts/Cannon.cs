c5949c03745964192bea426db257373e860bb6d8
Rishabh Dev Nayak <rdnayak26@gmail.com>
2024-04-05 18:51:15 +0530

    Attached Shot to Cannon(Testing)

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+    [SerializeField] private Transform fireshoot;
+    public Transform inpos;
+    public float y, z;
+    private float Power = 0f;
+    private float scrollspeed = 0.2f;
+    private Transform shot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inpos = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+        float scrollinput = 10 * Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollinput > 0) {
+            Power += scrollinput * scrollspeed;
+            Debug.LogError(scrollinput);
+        }
+        if (scrollinput < 0) {
+            Power += scrollinput * scrollspeed;
+            Debug.LogError(scrollinput);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            OnClickShoot();
+            Rigidbody rb = shot.GetComponent<Rigidbody>();
+            rb.AddForce(new Vector3(0, y * Power, z * Power), ForceMode.Impulse);
+        }
+    }
+
+    private void OnClickShoot() {
+        shot =  Instantiate(fireshoot, inpos.transform.position, inpos.transform.rotation);
     }
 }
--- Assets/Scripts/Cannon_Projectile.cs
+++ Assets/Scripts/Cannon_Projectile.cs
@@ -5,32 +5,16 @@ using UnityEngine;
 public class Cannon_Projectile : MonoBehaviour
 {
      
-    public Rigidbody rb;
-    public float y, z;
-    private float Power = 0f;
-    private float scrollspeed = 0.2f;
+    
 
     void Start()
     {
         
-        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float scrollinput = 10 * Input.GetAxis("Mouse ScrollWheel");
-
-        if ( scrollinput > 0) {
-            Power += scrollinput * scrollspeed;
-            Debug.LogError(scrollinput);
-        }
-        if (scrollinput < 0) {
-            Power += scrollinput * scrollspeed;
-            Debug.LogError(scrollinput);
-        }
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            rb.AddForce(new Vector3(0, y*Power, z*Power), ForceMode.Impulse);
-        }
+        
     }
 }
